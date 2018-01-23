81c3d38a17e71f97c2a7f99f982f737d23224462
Fatema Tuj Zohra <ftzohratit@gmail.com>
2018-01-23 16:28:03 -0500

    my first automation script using selenium java

---
+++ src/test/java/myfirstautomationscript/Facebook.java
@@ -0,0 +1,39 @@
+package myfirstautomationscript;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Facebook {
+
+	public static void main(String[] args) {
+		// Create a new instance of the Firefox driver
+   System.setProperty("webdriver.gecko.driver", "C:/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        
+        
+
+        
+        //driver.get("https://www.facebook.com");
+        driver.get("https://www.facebook.com");
+       
+        System.out.println(driver.getTitle());
+        //System.out.println(driver.getPageSource());
+        System.out.println(driver.getCurrentUrl());
+        
+       
+        
+
+        // Print a Log In message to the screen
+       // System.out.println("Successfully opened the facebook website");
+
+        //Wait for 6 Sec
+        //Thread.sleep(6000);
+
+        // Close the driver
+       // driver.close();
+
+	}
+
+}
