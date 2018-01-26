e5a380ce5f0b41eb2dca08c2f061f6c8bd480e15
Fatema Tuj Zohra <ftzohratit@gmail.com>
2018-01-25 21:06:10 -0500

    Automation of Radiobutton using customized XPath

---
+++ src/test/java/webelementsautomation/Radiobuttons_CustomizedXPath.java
@@ -0,0 +1,17 @@
+package webelementsautomation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Radiobuttons_CustomizedXPath {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "C:/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.keynotesupport.com/websites/contact-form-example-radio-buttons.shtml");
+		driver.findElement(By.xpath("//input[@value='QBP']")).click();
+
+	}
+
+}
